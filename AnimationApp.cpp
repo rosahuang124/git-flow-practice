e30951cb8af9ed39873256440910a7196f12742a
rosahuang124 <rosa@RosadeMBP.wireless.newschool.edu>
2017-03-01 19:39:32 -0500

    Modify

--- AnimationApp.cpp
+++ AnimationApp.cpp
@@ -14,19 +14,19 @@ class AnimationApp : public App {
 	void mouseDown( MouseEvent event ) override;
 	void update() override;
 	void draw() override;
-    
+
     ci::Anim<ci::vec2> subButtonOnePos;
     ci::Anim<ci::vec2> subButtonTwoPos;
     ci::Anim<ci::vec2> subButtonThreePos;
-    
+
     ci::Anim<ci::vec2> subButtonOnePos_after;
     ci::Anim<ci::vec2> subButtonTwoPos_after;
     ci::Anim<ci::vec2> subButtonThreePos_after;
-    
-    
-    int circleNum = 5;
+
+
+    int circleNum = 10;
     int size;
-    
+
 };
 
 void AnimationApp::setup()
@@ -37,11 +37,11 @@ void AnimationApp::setup()
     subButtonOnePos = getWindowCenter();
     subButtonTwoPos = getWindowCenter();
     subButtonThreePos = getWindowCenter();
-    
+
     subButtonOnePos_after = vec2(500,280);
     subButtonTwoPos_after = vec2(400,200);
     subButtonThreePos_after = vec2(300,280);
-    
+
 }
 
 
@@ -55,15 +55,15 @@ void AnimationApp::mouseDown( MouseEvent event )
         timeline().apply( &subButtonThreePos, (vec2)subButtonThreePos_after, 0.7f, EaseInOutCubic());
 
     };
-    
+
     if (event.isRight()){
         timeline().apply( &subButtonOnePos_after, getWindowCenter(), 0.7f, EaseInOutBack());
         timeline().apply( &subButtonTwoPos_after, getWindowCenter(), 1.0f, EaseInOutBack());
         timeline().apply( &subButtonThreePos_after, getWindowCenter(), 1.5f, EaseInOutBack());
     };
-    
-    
-    
+
+
+
 
 }
 
@@ -75,24 +75,24 @@ void AnimationApp::update()
 void AnimationApp::draw()
 {
 	gl::clear( Color( 0, 0, 0 ) );
-    
-    
+
+
     gl::color( Color(0.15f, 0.7f, 0.51f) );
     gl::drawSolidCircle( subButtonOnePos , 18 );
-    
+
     gl::color( Color(0.15f, 0.7f, 0.51f) );
     gl::drawSolidCircle( subButtonTwoPos , 18 );
-    
+
     gl::color( Color(0.15f, 0.7f, 0.51f) );
     gl::drawSolidCircle( subButtonThreePos , 18 );
-    
+
     gl::color( Color(0.9f, 0.3f, 0.5f) );
     gl::drawSolidCircle( getWindowCenter(), 30);
-    
-    
-   
-    
-    
+
+
+
+
+
 }
 
 CINDER_APP( AnimationApp, RendererGl )
